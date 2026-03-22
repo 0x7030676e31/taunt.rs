8f3a1d93284d9b0b5706c7ef9f22927f0ea6d4e9
sjeulu <sjeulu@proton.me>
2026-03-22 22:12:43 +0000

    package the frontend via bun2nix

---
+++ packages/frontend/default.nix
@@ -0,0 +1,29 @@
+{ stdenvNoCC, inputs, ... }:
+
+let
+  bun2nix = inputs.bun2nix.packages.${stdenvNoCC.hostPlatform.system}.bun2nix;
+
+in stdenvNoCC.mkDerivation {
+  pname = "taunt.rs-frontend";
+  version = "1.0.0";
+
+  src = ./.;
+
+  nativeBuildInputs = [
+    bun2nix.hook
+  ];
+
+  bunDeps = bun2nix.fetchBunDeps {
+    bunNix = ./bun.nix;
+  };
+
+  buildPhase = ''
+    bun run build \
+      --minify
+  '';
+
+  installPhase = ''
+    mkdir -p $out/dist
+    cp -R ./dist $out
+  '';
+}
