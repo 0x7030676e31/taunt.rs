5e221edc90991fc8d88c851041dcb28be08c3065
Tobiasz Chmielewski <0x7030676e31@proton.me>
2026-04-03 13:16:05 +0200

    feat(api): add public and sessions modules; update mod.rs and main.rs

--- packages/server/src/api/mod.rs
+++ packages/server/src/api/mod.rs
@@ -1,3 +1,4 @@
 pub mod error_response;
+pub mod public;
 pub mod routes;
 pub mod sessions;
--- packages/server/src/main.rs
+++ packages/server/src/main.rs
@@ -1,23 +1,24 @@
 #![feature(unwrap_infallible)]
 
-use std::{fs, sync::Arc};
+use std::fs;
 
 use actix_web::{App, HttpResponse, HttpServer, Responder, get, post, web};
 use log::{error, info, warn};
+use sqlx::{SqlitePool, sqlite::SqliteConnectOptions};
 
-use crate::configuration::AppConfiguration;
+use crate::{configuration::AppConfiguration, database::users::UsersTable};
 
 mod api;
 mod app;
 mod configuration;
 mod database;
 
-#[post("/")]
+#[get("/")]
 async fn index_page(config: web::Data<AppConfiguration>) -> HttpResponse {
     HttpResponse::Ok().body(fs::read(&config.static_asset_paths.index).unwrap_or("oopsie".into()))
 }
 
-#[actix_web::main()]
+#[actix_web::main]
 async fn main() -> Result<(), std::io::Error> {
     let config = web::Data::new(configuration::build_or_exit_with_error());
     let logger = colog::default_builder()
@@ -26,14 +27,26 @@ async fn main() -> Result<(), std::io::Error> {
     log::set_max_level(log::LevelFilter::max());
     log::set_boxed_logger(Box::new(logger))
         .expect("This should be the only logger set up in the main function");
+
     info!("Configuration:\n{}", config.as_ref());
+
+    let opt = SqliteConnectOptions::new().filename(&config.database_url.value);
+    let pool = SqlitePool::connect_with(opt)
+        .await
+        .expect("Failed to connect to the database");
+
+    let users_table = UsersTable::new(pool.clone());
+    let users_table = web::Data::new(users_table);
+
     let host_and_port = (config.host.value.clone(), config.port.value);
+
     HttpServer::new(move || {
         App::new()
             .service(index_page)
             .app_data(config.clone())
+            .app_data(users_table.clone())
     })
-        .bind(host_and_port)?
-        .run()
-        .await
+    .bind(host_and_port)?
+    .run()
+    .await
 }
