34d28d9279e33145756a20047dda3c927abedde9
Tobiasz Chmielewski <0x7030676e31@proton.me>
2026-04-03 18:21:07 +0200

    Enhance layout and styling for improved user experience; add error response handling

--- packages/frontend/src/pages/home/index.tsx
+++ packages/frontend/src/pages/home/index.tsx
@@ -1,7 +1,17 @@
+import { useI18n } from "@/locales/i18n";
+
 import styles from "./home.module.scss";
+import cat from "@/assets/cat_main.jpg";
 
 export default function Home() {
+    const [t] = useI18n();
+
     return (
-        <></>
+        <div class={styles.home}>
+            <div class={styles.catContainer}>
+                <img src={cat} alt="Cute cat" class={styles.catImage} />
+                <p class={styles.catCaption}>{t("home.catCaption")}</p>
+            </div>
+        </div>
     );
 }
