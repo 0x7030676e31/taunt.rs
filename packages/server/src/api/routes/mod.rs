1180785041085da663bfb565e3f8e7638cc9e464
Tobiasz Chmielewski <0x7030676e31@proton.me>
2026-04-03 14:41:35 +0200

    feat(api): add socket routes and update API routing structure

--- packages/server/src/api/routes/mod.rs
+++ packages/server/src/api/routes/mod.rs
@@ -1,7 +1,10 @@
 use actix_web::Scope;
 
 mod auth;
+mod socket;
 
 pub fn routes() -> Scope {
-    Scope::new("/api").service(auth::routes())
+    Scope::new("/api")
+        .service(auth::routes())
+        .service(socket::routes())
 }
